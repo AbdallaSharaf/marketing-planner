2fc8ebefd3e868398111120559d6eef6b1a163a0
Abdalla <abdallahsharaf95@gmail.com>
2025-11-12 15:28:50 +0200

    commit

--- tests/integration/health.test.js
+++ tests/integration/health.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../app');
+const app = require('../../src/app');
 
 describe('Health endpoint', () => {
   it('responds with status ok', async () => {
